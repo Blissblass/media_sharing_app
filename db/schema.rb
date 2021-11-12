d7c09c69691640a8257f7231342258955cb0dda9
Lily <lolpo12345@outlook.com>
2021-11-12 17:23:50 +0300

    Create Comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_12_134045) do
+ActiveRecord::Schema.define(version: 2021_11_12_135218) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.string "commentable_type"
+    t.integer "commentable_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
+  end
 
   create_table "likes", force: :cascade do |t|
     t.string "likeable_type"
