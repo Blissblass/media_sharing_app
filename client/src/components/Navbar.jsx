f7f243e9da222c1a7e53fee7b0a7c92e937590c7
Lily <lolpo12345@outlook.com>
2021-11-14 00:13:29 +0300

    Add Login.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = (props) => {
 
   return(
     <nav style={{backgroundColor: Colours.black}} 
-        className="navbar navbar-dark navbar-expand-lg justify-content-between align-items-center">
+        className="navbar navbar-dark navbar-expand-lg justify-content-between align-items-center shadow">
       <h3 style={{fontSize:40, color: Colours.white}}  className="navbar-brand m-3">Sound.io</h3>
       <NavbarButtons />
     </nav>
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Login from './components/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Navbar />
-    <Home />
-  </React.StrictMode>,
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+    </Switch>
+  </Router>,
   document.getElementById('root')
 );
