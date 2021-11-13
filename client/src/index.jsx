7f20dfedb5afe19ac7c8138a61dff51318547899
Lily <lolpo12345@outlook.com>
2021-11-13 22:19:19 +0300

    Set up basic file structure and set up imports

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './Home';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Navbar />
     <Home />
   </React.StrictMode>,
   document.getElementById('root')
