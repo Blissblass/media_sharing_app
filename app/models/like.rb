86a83909d140c243f87299c1c345d4d0ddd22ab7
Lily <lolpo12345@outlook.com>
2021-11-13 16:28:11 +0300

    Change Like model relation to make it more readable

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
 class Like < ApplicationRecord
-  belongs_to :liker, class_name: 'User'
   belongs_to :likeable, :polymorphic => true
+  belongs_to :user, class_name: 'User', primary_key: 'id', foreign_key: 'liker_id'
 end
