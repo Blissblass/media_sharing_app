1a63bbf9b3db3e6acac9e165bcab6ac5700fbe00
Lily <lolpo12345@outlook.com>
2021-11-13 23:14:00 +0300

    Create Colours.js

---
+++ client/src/components/Colours.js
@@ -0,0 +1,7 @@
+
+const Colours = {
+  black: '#343434',
+  white: '#F8F8F8'
+};
+
+export default Colours;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import './Navbar.css'
+import Colours from "./Colours";
 
 const Navbar = (props) => {
 
   return(
-    <div>
-      <h1>Navbar!</h1>
-    </div>
+    <nav style={{backgroundColor: Colours.black}} className="navbar navbar-dark navbar-expand-lg">
+      <h3 style={{fontSize:30, color: Colours.white}}  className="navbar-brand m-3">Sound.io</h3>
+    </nav>
   )
 };
 
