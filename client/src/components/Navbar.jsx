8ca076d2891b17d016737b0f919f2a5f884b75ab
Lily <lolpo12345@outlook.com>
2021-11-13 23:20:24 +0300

    Separate NavbarButtons

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import './Navbar.css'
 import Colours from "./Colours";
+import NavbarButtons from "./NavbarButtons";
 
 const Navbar = (props) => {
 
   return(
     <nav style={{backgroundColor: Colours.black}} className="navbar navbar-dark navbar-expand-lg">
-      <h3 style={{fontSize:30, color: Colours.white}}  className="navbar-brand m-3">Sound.io</h3>
+      <h3 style={{fontSize:40, color: Colours.white}}  className="navbar-brand m-3">Sound.io</h3>
+      <NavbarButtons />
     </nav>
   )
 };
---
+++ client/src/components/NavbarButtons.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+
+const NavbarButtons = () => {
+
+  return(
+    <div>
+      <Button>Log In</Button>
+      <Button>Sign Up</Button>
+    </div>
+  )
+};
+
+export default NavbarButtons;
