847b88b9d451ce45094e69e61d8904831a9d851f
Lily <lolpo12345@outlook.com>
2021-11-13 23:55:06 +0300

    Create UsersController for API namespace

---
+++ app/controllers/api/user_controller.rb
@@ -0,0 +1,12 @@
+class UserController < ApplicationController
+
+  def fetch_current_user
+    @user = current_user || nil 
+    if @user
+      render json: @user
+    else
+      render json: { msg: 'User not found!' }
+    end
+  end
+
+end
