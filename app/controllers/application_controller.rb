ad6f6344cb3dca3ea2585819c4721fd24c34bbd3
Lily <lolpo12345@outlook.com>
2021-11-14 02:19:00 +0300

    Add devise sanitiser

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up)  { |u| u.permit( :username, :email, :password, :password_confirmation ) }
+  end
 end
