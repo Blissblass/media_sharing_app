7d03a398058770df120975153c5299fbeeee25a4
Lily <lolpo12345@outlook.com>
2021-11-15 21:50:52 +0300

    Remove Username field from Login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -14,9 +14,8 @@ const Login = (props) => {
     const inputs = e.currentTarget;
     const data = {
       user: {
-        username: inputs[0].value,
-        email: inputs[1].value,
-        password: inputs[2].value,
+        email: inputs[0].value,
+        password: inputs[1].value,
       }
     }
 
@@ -51,10 +50,9 @@ const Login = (props) => {
       <div className="card container mt-4 me-4 p-4 w-50 float-end text-center shadow-lg">
         <h1>Log In</h1>
 
-        <form onSubmit={handleSubmit}>
-          <input type="text" className="form-control form-control-lg mt-2 shadow-sm" placeholder="Username..." />  
-          <input type="email" className="form-control form-control-lg mt-3 shadow-sm" placeholder="Email..." />  
-          <input type="password" className="form-control form-control-lg mt-3 shadow-sm" placeholder="Password..." />  
+        <form onSubmit={handleSubmit}> 
+          <input type="email" className="form-control form-control-lg mt-3 shadow-sm" placeholder="Email..." required />  
+          <input type="password" className="form-control form-control-lg mt-3 shadow-sm" placeholder="Password..." required />  
           <Button type="submit" variant="outline-primary" size="lg" className="me-5 ms-5 mt-4 w-50">Log In</Button>
         </form>
       </div>
