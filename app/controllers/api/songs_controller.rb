d73ca23e7e6b3c096d20ee7cd5d78a1db2d52994
Lily <lolpo12345@outlook.com>
2021-11-16 18:11:23 +0300

    Add user and song prefetching to SongsControllerg

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,7 +1,7 @@
 class Api::SongsController < ApplicationController
 
   def fetch_home_feed
-    songs_fetch = Song.all.includes(media_attachment: :blob).order(created_at: :desc)
+    songs_fetch = Song.all.with_attached_media.includes(:user).order(created_at: :desc)
     @songs = songs_fetch.map { |song| song.attributes.merge('media' => url_for(song.media)) }
     render json: @songs
   end
--- client/src/components/Song.jsx
+++ client/src/components/Song.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import Colours from './Colours';
+import { FiThumbsUp } from 'react-icons/fi';
 
 const Song = (props) => {
 
   return(
     <div className="card container w-50 mt-4" style={{backgroundColor: Colours.white}}>
-      <h3>{props.song.title}</h3>
-      <audio controls style={{}}> 
-        <source src={props.song.media} /> 
-      </audio>
+      <div className="p-4" style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>      
+        <div className="px-2" style={{display: 'flex', alignItems: 'center'}}>
+          <FiThumbsUp className="mb-1 me-5" style={{fontSize: 40, cursor: "pointer"}} />
+          <h3>{props.song.title}</h3>
+        </div>
+        <audio controls> 
+          <source src={props.song.media} /> 
+        </audio>
+      </div>
     </div>
   )
 };
