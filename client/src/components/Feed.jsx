25de0bdec6b92fca7d6cd547f75b566c743911c7
Lily <lolpo12345@outlook.com>
2021-11-17 02:54:03 +0300

    Make Feed more readable

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,20 +1,27 @@
-import React, { useLayoutEffect } from "react";
+import React, { useLayoutEffect, useState } from "react";
 import Song from './Song';
 import { Stack } from 'react-bootstrap';
 
 const Feed = (props) => {
-  
+  const [loading, setLoading] = useState(false);
 
   useLayoutEffect(() => {
-    fetch('/api/home_feed')
-      .then(data => data.json())
-      .then(data => props.setSongs(data));
+    const fethData = async () => { 
+      fetch('/api/home_feed')
+        .then(data => data.json())
+        .then(data => props.setSongs(data));
+    }
   }, []);
 
   return(
     <div className="mt-3">
       <Stack gap={4}>
-        { props.songs.length > 0 ? props.songs.map(song => <Song key={song.id} song={song}/>) : <h1 className="text-center text-white mt-4">No songs available.</h1> }
+        { props.songs.length > 0 ? 
+
+          props.songs.map(song => <Song key={song.id} song={song}/>) 
+        : 
+          <h1 className="text-center text-white mt-4">No songs available.</h1> 
+        }
       </Stack>
     </div>
   )
