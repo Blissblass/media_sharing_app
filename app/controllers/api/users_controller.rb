656666118839f426f2c355e03dfdce93a493485e
Lily <lolpo12345@outlook.com>
2021-11-19 23:09:12 +0300

    Update API::SongsController

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,7 +2,7 @@ class Api::UsersController < ApplicationController
 
   def fetch_user_profile
     @user = User.find(params[:id])
-    render json: @user
+    render json: { user: @user, likes: @user.likes, songs: @user.songs }
   end
 
 end
