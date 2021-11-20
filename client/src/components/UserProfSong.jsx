936ba4ecf492b14d7495a29a679e2989345baa33
Lily <lolpo12345@outlook.com>
2021-11-20 23:49:03 +0300

    Create ProfSongInfo.jsx

--- client/src/components/UserProfSong.jsx
+++ client/src/components/UserProfSong.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
+import UserContext from "./UserContext";
+import Colours from './Colours';
+import SongInfo from './SongInfo';
+import { BiTrash } from 'react-icons/bi';
 
 const UserProfSong = (props) => {
-  const song = props.song;
+  const { user } = useContext(UserContext); 
+
+  const handleDelete = () => {
+    fetch(`/songs/${props.song.id}`, {
+      method: 'DELETE'
+    })
+
+    props.setPosts(oldPosts => oldPosts.filter(post => post.id !== props.song.id))
+  };
 
   return(
-    <div>
-      <audio controls>
-        <source src={song.media} />
-      </audio>
+    <div className="card container w-75 mt-4" style={{backgroundColor: Colours.white}}>
+      <div className="p-4" style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>      
+        <div>
+          <SongInfo song={props.song} title={props.song.title} user={props.song.user} currUser={user} likes={props.song.likes} 
+            setLoading={props.setLoading} />
+        </div>
+        <audio controls style={{width: 650}}> 
+          <source src={props.song.media} /> 
+        </audio>
+
+        { user.id === props.song.user.id ? 
+          <BiTrash style={{fontSize: 40, cursor: 'pointer'}} onClick={handleDelete} />
+        :
+          null
+        }
+      </div>
     </div>
   )
 };
