2767eec74dcbeeef4933bc3ce811cb9b8f7290ab
Lily <lolpo12345@outlook.com>
2021-11-21 19:56:21 +0300

    Remove old code

--- app/models/comment.rb
+++
@@ -1,5 +0,0 @@
-class Comment < ApplicationRecord
-  belongs_to :commentable, :polymorphic => true
-  belongs_to :commenter, class_name: 'User'
-  has_many :likes, :as => :likeable
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :songs       
-  has_many :sent_comments, class_name: 'Comment', foreign_key: 'commenter_id'
-  has_many :comments, :as => :commentable
   has_many :likes, class_name: 'Like', foreign_key: 'liker_id'
   has_many :liked_songs, :through => :likes, :source => :likeable, :source_type => 'Song'
 end
