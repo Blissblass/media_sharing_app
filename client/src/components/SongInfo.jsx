da979989a447ede6a3ffb53e4982ca5e1aae2d46
Lily <lolpo12345@outlook.com>
2022-04-12 22:14:14 +0300

    Make UI updates for song likes asnychnorous

--- client/src/components/SongInfo.jsx
+++ client/src/components/SongInfo.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Link } from 'react-router-dom';
-import { FaRegThumbsUp} from 'react-icons/fa';
+import { Link } from "react-router-dom";
+import { FaRegThumbsUp } from "react-icons/fa";
 
 const SongInfo = (props) => {
   const [likes, setLikes] = useState(props.likes);
@@ -9,75 +9,93 @@ const SongInfo = (props) => {
   const { setLoading, song, currUser } = props;
 
   useEffect(() => {
-    const fetchLiked = async () => { 
-        fetch('https://sound-io-backend.herokuapp.com/api/already_liked', {
-        method: 'POST',      
+    const fetchLiked = async () => {
+      fetch("https://sound-io-backend.herokuapp.com/api/already_liked", {
+        method: "POST",
         headers: new Headers({
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
+          "Content-Type": "application/json",
+          Accept: "application/json",
         }),
-        body: JSON.stringify({id: song.id, liker_id: currUser.id})
-      })      
-      .then(data => data.json())
-      .then(data => {
-        if(data.length > 0) {
-          setLiked(true);
-          setLikeData(data[0]);
-        }
-        setLoading(false);
-      });
-    }
+        body: JSON.stringify({ id: song.id, liker_id: currUser.id }),
+      })
+        .then((data) => data.json())
+        .then((data) => {
+          if (data.length > 0) {
+            setLiked(true);
+            setLikeData(data[0]);
+          }
+          setLoading(false);
+        });
+    };
 
-    fetchLiked()
+    fetchLiked();
   }, [setLoading, currUser.id, song.id]);
-  
+
   const handleLike = () => {
     const data = {
       like: {
-        type: 'Song',
+        type: "Song",
         liker_id: props.currUser.id,
-        id: props.song.id
-      }
-    }
+        id: props.song.id,
+      },
+    };
 
-    if(liked) {
-
+    if (liked) {
       fetch(`https://sound-io-backend.herokuapp.com/likes/${likeData.id}`, {
-        method: 'DELETE'
-      })
+        method: "DELETE",
+      });
       setLiked(false);
-      setLikes(old => old -= 1);
-
+      setLikes((old) => (old -= 1));
     } else {
-      fetch('https://sound-io-backend.herokuapp.com/likes', {
-        method: 'POST',      
+      fetch("https://sound-io-backend.herokuapp.com/likes", {
+        method: "POST",
         headers: new Headers({
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
+          "Content-Type": "application/json",
+          Accept: "application/json",
         }),
-        body: JSON.stringify(data)
+        body: JSON.stringify(data),
       })
-      .then(data => data.json())
-      .then(data => setLikeData(data));
+        .then((data) => data.json())
+        .then((data) => setLikeData(data));
       setLiked(true);
-      setLikes(old => old += 1);
+      setLikes((old) => (old += 1));
     }
   };
 
-  return(
-    <div className="px-2" style={{display: 'flex', alignItems: 'center'}}>
-      <div style={{display:'flex', alignItems:'center', justifyContent: 'space-between'}}>
-        <h2 className="mb-1 me-2" >{likes}</h2>
-        <FaRegThumbsUp className="mb-2" 
-          style={{fontSize: 40, cursor: "pointer", color: liked ? "#087cfc" : "#302c34", marginRight: 60}} 
-          onClick={handleLike} />
+  return (
+    <div className="px-2" style={{ display: "flex", alignItems: "center" }}>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <h2 className="mb-1 me-2">{likes}</h2>
+        <FaRegThumbsUp
+          className="mb-2"
+          style={{
+            fontSize: 40,
+            cursor: "pointer",
+            color: liked ? "#087cfc" : "#302c34",
+            marginRight: 60,
+          }}
+          onClick={handleLike}
+        />
       </div>
       <div>
         <h2>{props.title}</h2>
-        <h4><Link className="text-secondary text-decoration-none" to={`/user/${props.user.id}`}>{props.user.username}</Link></h4>
+        <h4>
+          <Link
+            className="text-secondary text-decoration-none"
+            to={`/user/${props.user.id}`}
+          >
+            {props.user.username}
+          </Link>
+        </h4>
       </div>
     </div>
-  )
+  );
 };
 
 export default SongInfo;
